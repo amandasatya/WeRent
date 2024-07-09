72ce6e39822b95f225ff819177dbafe51e0463a2
amandasatya <amandasatya93@gmail.com>
2024-07-09 19:41:43 +0700

    table rate

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -5,42 +5,41 @@ import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductService {
-    constructor ( private prismaService: PrismaService ) {}
+  constructor(private prismaService: PrismaService) {}
 
-    async create(dto: CreateProductDto) {
-        const { sizes, ...rest } = dto;
-        return this.prismaService.product.create({
-            data: {
-                ...rest,
-                sizes: { set: sizes },
-          },
-        });
+  async create(dto: CreateProductDto) {
+    const { sizes, ...rest } = dto;
+    return this.prismaService.product.create({
+      data: {
+        ...rest,
+        sizes: { set: sizes },
+      },
+    });
+  }
 
-    }
+  async findAll() {
+    return this.prismaService.product.findMany();
+  }
 
-    async findAll() {
-        return this.prismaService.product.findMany();
-    }
-    
-    async findOne(id: number) {
-        return this.prismaService.product.findUnique({
-          where: { product_id: id },
-        });
-    }
-    
-    async update(id: number, updateProductDto: UpdateProductDto) {
-        const { sizes, ...rest } = updateProductDto;
-        return this.prismaService.product.update({
-          where: { product_id: id },
-          data: {
-            ...rest,
-          },
-        });
-    }
-    
-    async remove(id: number) {
-        return this.prismaService.product.delete({
-          where: { product_id: id },
-        });
-    }
+  async findOne(id: number) {
+    return this.prismaService.product.findUnique({
+      where: { product_id: id },
+    });
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const { sizes, ...rest } = updateProductDto;
+    return this.prismaService.product.update({
+      where: { product_id: id },
+      data: {
+        ...rest,
+      },
+    });
+  }
+
+  async remove(id: number) {
+    return this.prismaService.product.delete({
+      where: { product_id: id },
+    });
+  }
 }
