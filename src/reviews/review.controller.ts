ad6e7ceb85f2b3d84680c42563254cb45cc03ebf
Sharon Silalahi <aljeazsharon@gmail.com>
2024-07-17 14:32:57 +0700

    feat. add HttpCode & HttpStatus for Review & Like Table

--- src/reviews/review.controller.ts
+++ src/reviews/review.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards, HttpCode, HttpStatus, NotFoundException } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { Review } from '@prisma/client';
 import { CreateReviewDto } from './dto/review-create.dto';
@@ -13,30 +13,46 @@ export class ReviewController {
 
   @UseGuards(JwtAuthGuard)
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   async createReview(@Body() createReviewDto: CreateReviewDto, @User() user): Promise<Review> {
     return this.reviewService.createReview(createReviewDto, user);
   }
 
-
   @Get()
+  @HttpCode(HttpStatus.OK)
   async getReviews(): Promise<Review[]> {
     return this.reviewService.getReviews();
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   async getReviewById(@Param('id') id: string): Promise<Review> {
-    return this.reviewService.getReviewById(+id);
+    const review = await this.reviewService.getReviewById(+id);
+    if(!review) {
+      throw new NotFoundException("Review Not Found!");
+    }
+    return review;
   }
 
   @UseGuards(JwtAuthGuard)
   @Put(':id')
+  @HttpCode(HttpStatus.OK)
   async updateReview(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto): Promise<Review> {
-    return this.reviewService.updateReview(+id, updateReviewDto);
+    const review = await this.reviewService.updateReview(+id, updateReviewDto);
+    if (!review) {
+      throw new NotFoundException("Review Not Found!");
+    }
+    return review;
   }
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async deleteReview(@Param('id') id: string): Promise<Review> {
-    return this.reviewService.deleteReview(+id);
+    const review = await this.reviewService.deleteReview(+id);
+    if (!review) {
+      throw new NotFoundException("Review Not Found!")
+    }
+    return review;
   }
 }
